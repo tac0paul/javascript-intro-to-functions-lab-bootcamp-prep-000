666310b539cbecaf1e6a600d82e187dec772ece9
paulo.gomes@owl.ucc.edu <paulo.gomes@owl.ucc.edu>
2018-05-15 00:39:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase())
-  return ("I can't hear you!"); }
+  return ("I can't hear you!"); 
   
   if (string.toUpperCase())
   return ("YES INDEED!");
