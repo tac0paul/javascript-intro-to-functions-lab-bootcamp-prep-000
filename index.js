69669ad8627fb16f65168e6c9e385c963c3e210f
paulo.gomes@owl.ucc.edu <paulo.gomes@owl.ucc.edu>
2018-05-15 01:32:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string) {
   return (`YES INDEED!`);
   
  else if (string === string.toLowerCase())
-  return (`I can't hear you`)
+  return (`I can't hear you!`)
   
 else if (string === ("I love you, Grandma"))
   return (`I love you, too`) }
